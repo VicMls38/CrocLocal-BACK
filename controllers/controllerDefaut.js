04a5f07132487f6d554f5c125e8ec30238b8e65a
Victor <victor.morales@outlook.fr>
2024-03-30 22:47:54 +0100

    Update for db con test

--- controllers/controllerDefaut.js
+++ controllers/controllerDefaut.js
@@ -1,16 +1,23 @@
-//Importation de la connexion à la bdd
-const bodyParser = require('body-parser');
-//Importation du fichier models
-//var Model = require('../models/modelDefaut');
-
+// Importation du modèle userModel
+var userModel = require('../models/modelDefaut');
 
 module.exports = {
-
     // Redirection vers l'accueil
-    Accueil : (req, res) => {
-            res.json({Code:"Hello !"});
+    Accueil: function(req, res) {
+        res.json({ code: "Hello !" });
     },
 
-   
-
-}
+    // Fonction pour récupérer tous les utilisateurs
+    getUsers: function(req, res) {
+        // Appel de la fonction pour récupérer tous les utilisateurs du modèle userModel
+        userModel.getAllUsers(function(err, users) {
+            if (err) {
+                // En cas d'erreur, renvoyer un message d'erreur au client
+                return res.status(500).json({ error: "Erreur lors de la récupération des utilisateurs." });
+            } else {
+                // En cas de succès, renvoyer les utilisateurs récupérés au client
+                return res.json(users);
+            }
+        });
+    }
+};
