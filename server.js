61f4bb13eba8f79d0a13400a64ab2850bf7bf3ec
Victor MORALES <victor.morales@outlook.fr>
2024-11-26 11:20:05 +0100

    commit for checkout, jwt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 var bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv')
+const middleware = require('./middleware')
 
 const jwt = require('jsonwebtoken')
 
@@ -20,12 +21,13 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(require('body-parser').json())
 
-
-/*----- ROUTES -----*/
+/*----- ROUTEURS -----*/
 
 const RouteurDefaut = require('./routes/routeDefaut')
 
-/*-----------------*/
+
+
+/* PUBLIC ROUTE */
 
 
 app.use('/', RouteurDefaut);
@@ -33,57 +35,13 @@ app.use('/', RouteurDefaut);
 /* PRIVATE ROUTE */
 
 app.use('/user', (req, res, next)=> {
-  authMiddleware(req, res, next)
+  middleware.authMiddleware(req, res, next)
 })
 
 /*----------------------*/
 
 
-function authMiddleware(req, res, next) {
-  if (req && req.headers && req.headers.authorization) {
-      const token = req.headers.authorization;
-      if (token) {
-          const decodedToken = authenticateToken(token);
-          if (decodedToken) {
-              // req.userId = decodedToken.userId;
-              req.newToken = generateJWT(decodedToken.userId);
-              next();
-          } else {
-              console.log("Could not decode token");
-              res.sendStatus(500);
-          }
-      } else {
-          console.log("No token found")
-          res.sendStatus(400);
-      }
-  } else {
-      console.log("Issue in request for token")
-      res.sendStatus(400);
-  }
-}
 
-
-function authenticateToken(completeToken) {
-  const token = completeToken && completeToken.split(' ')[1]
-
-  if (token == null) {
-      console.log("Error reading token : NO TOKEN");
-      return false;
-  }
-
-  return jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-          console.log("Error checking token :", err);
-          return false;
-      }
-      return decoded;
-  })
-}
-
-function generateJWT(userId) {
-  const payload = { userId: userId };
-  return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
-}
 
 
 const port = 3000
