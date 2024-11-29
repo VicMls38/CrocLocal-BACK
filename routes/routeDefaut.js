9a4d4574d1525afff6c8506da3b6a3b3f061c340
Victor MORALES <victor.morales@outlook.fr>
2024-11-29 15:06:01 +0100

    ça marche

--- routes/routeDefaut.js
+++ routes/routeDefaut.js
@@ -7,9 +7,9 @@ const routeur = express.Router();
 
 routeur.get('/', Controller.Accueil);
 
-routeur.get('/secure/p', authMiddleware, authorize['producteur'], Controller.testP);
+routeur.get('/secure/p', authMiddleware, authorize(['producteur']), Controller.testP);
 
-routeur.get('/secure/c', authMiddleware, authorize['consommateur'], Controller.testC);
+routeur.get('/secure/c', authMiddleware, authorize(['consommateur']), Controller.testC);
 
 
 
